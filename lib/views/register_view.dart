12096cde5f7da1ad4194f5be53c1ffdd5a2535ed
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2024-01-10 00:16:28 -0600

    register view creation

---
+++ lib/views/register_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:busme/constants/styles/app_text_styles.dart';
+import 'package:busme/constants/styles/app_colors.dart';
+import 'package:sizer/sizer.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
